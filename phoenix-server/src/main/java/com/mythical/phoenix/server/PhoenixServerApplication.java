f3f28ee45341dd9d0193975b97c57592f8eb7a50
zhn4528 <zhn4528@rd.netease.com>
2022-02-10 14:23:29 +0800

    save

---
+++ phoenix-server/src/main/java/com/mythical/phoenix/server/PhoenixServerApplication.java
@@ -0,0 +1,17 @@
+package com.mythical.phoenix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: zhn4528
+ * create: 2022/2/10 13:51
+*/
+@SpringBootApplication
+public class PhoenixServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixServerApplication.class, args);
+    }
+
+}
